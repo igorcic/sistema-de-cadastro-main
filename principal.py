c3b526c8bd31765a27dca3e56e151a99066d091b
igor bastos <igorbastoscic@gmail.com>
2022-09-10 22:21:04 -0300

    Adicionando Arquivos de imagens + apagando algumas coisas do kivy que eram inuteis

--- principal.py
+++ principal.py
@@ -31,22 +31,6 @@ class Cozinha(Screen):
 class Gerente(Screen):
     pass
 
-class Tarefas(Screen):
-    def __init__(self,tarefas=[],**kwargs):
-        super().__init__(**kwargs)
-        for tarefa in tarefas:
-            self.ids.box.add_widget(Tarefa(text=tarefa))
-
-    def addWidget(self):
-        texto = self.ids.texto.text
-        self.ids.box.add_widget(Tarefa(text=texto))
-        self.ids.texto.text = ''
-
-class Tarefa(BoxLayout):
-    def __init__(self,text='',**kwargs):
-        super().__init__(**kwargs)
-        self.ids.label.text = text
-
 class Test3(App):
     def build(self):
         return Gerenciador()
